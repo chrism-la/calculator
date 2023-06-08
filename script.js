1dc643c6cf74e40d96b3a037091a0ab762b58e89
Chris <117781333+chrism-la@users.noreply.github.com>
2023-06-08 13:38:57 -0700

    updated updateDisplay function to clear out previous operand

--- script.js
+++ script.js
@@ -81,6 +81,8 @@ class Calculator {
       if(this.operation != null) {
          this.previousOperandTextElement.innerText = 
          `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+      } else {
+         this.previousOperandTextElement.innerText = ''
       }
    }
 }
